231eca961714367d754964559bc8dac31c8c07d7
Richmond Starbuck <rbstarbuck@gmail.com>
2016-12-13 22:02:18 -0500

    Added display controls for standings chart

--- core/db.service.js
+++ core/db.service.js
@@ -6,36 +6,46 @@ function DbService($q, $http) {
 
     var dbCache = null;
     self.getDb = function() {
-        if (!dbCache) {
+        if (!dbCache) { // first getDb() call, initialize cache
             deferred = $q.defer();
             dbCache = deferred.promise;
 
+            // creates new db if none exists, else opens existing db
             var db = openDatabase('team21db', '1.0', 'EECS 493 Team 21 DB', 2 * 1024 * 1024);
 
+            // get db initialization data
             $q.all([
                 $http.get('core/users.data.json'),
                 $http.get('core/dorms.data.json')
+            // fill tables with initialization data
             ]).then(function(data) {
                 var users = data[0].data.data;
                 var dorms = data[1].data.data;
                 
+                // perform SQL queries to make tables
                 db.transaction(function(tx) {
-                    tx.executeSql('CREATE TABLE IF NOT EXISTS Users (username PRIMARY KEY, password, name, bottleCount, canCount, boxCount)');
+                    // make and fill Users table
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS Users (username PRIMARY KEY, password, name, dorm, bottleCount, canCount, boxCount)');
                     for (var i = 0; i < users.length; ++i) {
                         var user = users[i];
-                        tx.executeSql('INSERT INTO Users VALUES (?, ?, ?, ?, ?, ?)', [user.username, user.password, user.name, user.recyclables.bottles, user.recyclables.cans, user.recyclables.boxes]);
+                        tx.executeSql('INSERT INTO Users VALUES (?, ?, ?, ?, ?, ?, ?)',
+                            [user.username, user.password, user.name, user.dorm,
+                             user.recyclables.bottles, user.recyclables.cans, user.recyclables.boxes]
+                        );
                     }
 
+                    // make and fill Dorms table
                     tx.executeSql('CREATE TABLE IF NOT EXISTS Dorms (name PRIMARY KEY)');
                     for (var i = 0; i < dorms.length; ++i) {
                         var dorm = dorms[i];
                         tx.executeSql('INSERT INTO Dorms VALUES (?)', [dorm]);
                     }
 
+                    // done, resolve promise
                     deferred.resolve(db);
                 });
+            // log error getting local JSON files if they occur
             }).catch(function(e) {
-                // error getting local JSON files
                 console.log(e);
                 deferred.reject(e);
             });
@@ -49,17 +59,24 @@ function DbService($q, $http) {
 
         self.getDb()
         .then(function(db) {
+            // convenience, allow string to be passed when only one prepared statement param is needed
             if (typeof(params) === 'string') {
                 params = [params];
             }
+
+            // perform SQL query
             db.transaction(function(tx) {
                 tx.executeSql(q, params, function(tx, results) {
+                    // build an array from results (WebSQL results are ugly, let's clean them up)
                     var arr = [];
+
+                    // if one column filter, make one array of values
                     if (typeof(filterValue) === 'string') {
                         for (var i = 0; i < results.rows.length; ++i) {
                             arr.push(results.rows.item(i)[filterValue]);
                         }
                     }
+                    // if multiple column filters, make an array of arrays of values
                     else if (typeof(filterValue) === 'object') {
                         for (var j = 0; j < filterValue.length; ++j) {
                             var values = [];
@@ -70,12 +87,16 @@ function DbService($q, $http) {
                             arr.push(values);
                         }
                     }
+                    // if no column filters, make one array of keys and values
                     else {
                         for (var i = 0; i < results.rows.length; ++i) {
                             arr.push(results.rows.item(i));
                         }
                     }
+
+                    // done, resolve promise
                     deferred.resolve(arr);
+                // log SQL query errors if they occur
                 }, function(tx, err) {
                     console.log(err);
                     deferred.reject(err);
--- standings/standings.component.js
+++ standings/standings.component.js
@@ -1,54 +1,114 @@
 var team21App = angular.module('team21App');
 
-function StandingsController($q, DbService) {
-	var self = this;
 
-	// TODO: delete below
-	self.users = "Loading";
-	self.dorms = "Loading";
-
-	DbService.query(
-		"SELECT * FROM Users ORDER BY ? + ? + ? DESC", // the query string
-		["bottleCount", "canCount", "boxCount"], // optional: replaces any '?' in query string (string or array)
-		["bottleCount", "canCount", "boxCount"]) // optional: returned array will contain the values (not keys) associated with these keys
-												 //   if an array of keys is passed, an array (by key) of arrays (by row) is returned
-												 //   if a single key (string) is passed, a single array of values is returned
-	.then(function(data) {
-		self.users = JSON.stringify(data);
-	});
-
-	DbService.query("SELECT * FROM Dorms")
-	.then(function(data) {
-		self.dorms = JSON.stringify(data);
-	});
-	// TODO: delete above
-
-	self.chartOptions = {
+var initStandingsController = function(ctrl, DbService) {
+	ctrl.chartOptions = {
 		scales: {
 			xAxes: [{
-			  stacked: true,
+				stacked: true,
 			}],
 			yAxes: [{
-			  stacked: true
+				stacked: true
 			}]
 		}
 	}
 
-	self.chartLabels = [];
-	self.chartData = [];
-	self.chartSeries = ['bottleCount', 'canCount', 'boxCount'];
-	self.orderBy = 'bottleCount + canCount + boxCount DESC';
+	ctrl.chartLabels = [];
+	ctrl.chartData = [];
+	ctrl.chartSeries = ['bottleCount', 'canCount', 'boxCount'];
+	ctrl.orderBy = 'bottleCount + canCount + boxCount DESC';
 
-	var getChartData = function() {
-		DbService.query('SELECT * FROM Users ORDER BY ?', self.orderBy, ['name', 'bottleCount', 'canCount', 'boxCount'])
-		.then(function(data) {
-			self.chartLabels = data[0];
-			self.chartData = [data[1], data[2], data[3]];
-		});
-	}
+	ctrl.dataSources = [
+		'Dorms',
+		'Individuals'
+	];
 
-	getChartData();
+	ctrl.dorms = ['Any Dorm'];	
+	DbService.query('SELECT dorm FROM Users GROUP BY dorm', [], 'dorm')
+	.then(function(data) {
+		ctrl.dorms = ctrl.dorms.concat(data);
+	});
+
+	ctrl.orderBys = [
+		'Score',
+		'Name'
+	];
+
+	ctrl.dataSource = ctrl.dataSources[0];
+	ctrl.dorm = ctrl.dorms[0];
+	ctrl.orderBy = ctrl.orderBys[0];
+	ctrl.orderByIsReversed = false;
+
+	return ctrl;
 }
+
+
+function StandingsController($q, DbService) {
+	var ctrl = initStandingsController(this, DbService);
+	
+
+	// TODO: delete below
+	ctrl.usersString = "Loading";
+	ctrl.dormsString = "Loading";
+
+	DbService.query(
+		"SELECT * FROM Users ORDER BY ? + ? + ? DESC", // the query string, standard SQL syntax
+		["bottleCount", "canCount", "boxCount"], // optional: replaces any '?' in query string (pass a string or array)
+												 //   WebSQL doesn't like '?' in some places, so be careful (just use string concats if it doesn't work)
+		["bottleCount", "canCount", "boxCount"]) // optional: returned array will contain the values (not keys) associated with these keys
+												 //   if an array of keys is passed, an array (by key) of arrays (values by row) is returned
+												 //   if a single key (string) is passed, a single array of values is returned
+	.then(function(data) {  // returns a promise; use .then() for callback, .catch() for handling error
+		ctrl.usersString = JSON.stringify(data);
+	});
+
+	DbService.query("SELECT * FROM Dorms", [], 'name')
+	.then(function(data) {
+		ctrl.dormsString = JSON.stringify(data);
+	});
+	// TODO: delete above
+
+
+	(ctrl.invalidateChart = function() {
+		// build SELECT statement
+		switch (ctrl.dataSource) {
+			case 'Dorms':
+				var query = 'SELECT dorm AS name, SUM(bottleCount) AS bottleCount, SUM(canCount) AS canCount, SUM(boxCount) AS boxCount, (SUM(bottleCount) + SUM(canCount) + SUM(boxCount)) AS totalCount FROM Users GROUP BY dorm';
+				break;
+
+			default: // case 'Individuals'
+				var query = 'SELECT name, bottleCount, canCount, boxCount, (bottleCount + canCount + boxCount) AS totalCount FROM Users';
+				if (ctrl.dorm !== 'Any Dorm') {
+					query += ' WHERE dorm = "' + ctrl.dorm + '"';
+				}
+				break;
+		}
+
+		// concat ORDER BY clause
+		switch (ctrl.orderBy) {
+			case 'Name':
+				query += ' ORDER BY name';
+				query += (ctrl.orderByIsReversed ? ' DESC' : ' ASC');
+				break;
+
+			default: // case 'Score'
+				query += ' ORDER BY totalCount';
+				query += (ctrl.orderByIsReversed ? ' ASC' : ' DESC');
+				break;
+		}
+		
+		// format query return value
+		var keys = ['name', 'bottleCount', 'canCount', 'boxCount'];
+
+		// query for chart data
+		DbService.query(query, [], keys)
+		.then(function(data) {
+			ctrl.chartLabels = data[0]; // keys[0]
+			ctrl.chartData = [data[1], data[2], data[3]]; // keys[1-3]
+		});
+	})(); // execute on load
+}
+
 
 team21App.component('standings', {
 	templateUrl: 'standings/standings.template.html',
